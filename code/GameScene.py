ed7ef08aefbbef5ef1f98bdc08f9e18cb1be0237
guisenges@poli.ufrj.br <guisenges@poli.ufrj.br@53eebc65-bcf8-1c7c-f537-236fe9c61844>
2012-03-24 19:09:37 +0000

    Adding collision and physics ignition. Setting global gravity force.

--- code/GameScene.py
+++ code/GameScene.py
@@ -2,7 +2,7 @@
 Created on 24/03/2012
 
 '''
-from pandac.PandaModules import CollisionHandlerQueue, CollisionNode, CollisionSphere, CollisionTraverser
+from pandac.PandaModules import *
 from direct.showbase.DirectObject import DirectObject
 
 import direct.directbase.DirectStart
@@ -10,12 +10,35 @@ import direct.directbase.DirectStart
 class World(DirectObject):
     def __init__(self):
         
+        #** Collision system ignition - even if we're going to interact with the physics routines, the usual traverser is always in charge to drive collisions
+        base.cTrav=CollisionTraverser()
+        # look here: we enable the particle system - this is the evidence of what I was saying above, because the panda physics engine is conceived mainly to manage particles.
+        base.enableParticles()
+        # here there is the handler to use this time to manage collisions.
+        collisionHandler = PhysicsCollisionHandler()
+        
+        #** This is the first time we see this collider: it is used mainly to define a flat infinite plane surface
+        cp = CollisionPlane(Plane(Vec3(0, 0, 1), Point3(0, 0, 0)))
+        planeNP = base.render.attachNewNode(CollisionNode('planecnode'))
+        planeNP.node().addSolid(cp)
+        planeNP.show()
+        
+        #** This is how to define the gravity force to make our stuff fall down: first off we define a ForceNode and attach to the render, so that everything below will be affected by this force
+        globalforcesFN=ForceNode('world-forces')
+        globalforcesFNP=base.render.attachNewNode(globalforcesFN)
+        # then we set a linear force that will act in Z axis-drag-down-force of 9.81 units per second.
+        globalforcesGravity=LinearVectorForce(0,0,-9.81)
+        globalforcesFN.addForce(globalforcesGravity)
+        # and then we assign this force to the physics manager. By the way, we never defined that manager, but it was made automatically when we called base.enableParticles()
+        base.physicsMgr.addLinearForce(globalforcesGravity)
+        
         # loading student_chair model 
         self.studentChairModel = loader.loadModel("../models/student_chair")
         studentChairCollider = self.studentChairModel.attachNewNode(CollisionNode('student_chaircnode'))
         studentChairCollider.node().addSolid(CollisionSphere(0, 0, 0, 1))
         self.studentChairModel.reparentTo(render)
         self.studentChairModel.setPos(-2, 25,0)
+        
 
 # now we may start the game
 world = World();
