0a5494e0f4b1e5e428f33b99cb911523237cdb8a
guisenges@poli.ufrj.br <guisenges@poli.ufrj.br@53eebc65-bcf8-1c7c-f537-236fe9c61844>
2012-04-03 03:52:53 +0000

    Removing code folder. Now everthing is inside src folder. Adding physics and collision changes to main.

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -35,13 +35,9 @@ class Main(ShowBase):
         # look here: we enable the particle system - this is the evidence of what I was saying above, because the panda physics engine is conceived mainly to manage particles.
         self.enableParticles()
         # here there is the handler to use this time to manage collisions.
-        collisionHandler = PhysicsCollisionHandler()
-        
-        #** This is the first time we see this collider: it is used mainly to define a flat infinite plane surface
-        cp = CollisionPlane(Plane(Vec3(0, 0, 1), Point3(0, 0, 0)))
-        planeNP = self.render.attachNewNode(CollisionNode('planecnode'))
-        planeNP.node().addSolid(cp)
-        planeNP.show()
+        collisionHandler = CollisionHandlerEvent()
+        # physical collisions
+        physicsHandler = PhysicsCollisionHandler()
         
         #** This is how to define the gravity force to make our stuff fall down: first off we define a ForceNode and attach to the render, so that everything below will be affected by this force
         globalforcesFN=ForceNode('world-forces')
@@ -52,12 +48,55 @@ class Main(ShowBase):
         # and then we assign this force to the physics manager. By the way, we never defined that manager, but it was made automatically when we called base.enableParticles()
         self.physicsMgr.addLinearForce(globalforcesGravity)
         
-        # loading student_chair model 
+        #** This is the first time we see this collider: it is used mainly to define a flat infinite plane surface
+        cp = CollisionPlane(Plane(Vec3(0, 0, 1), Point3(0, 0, 0)))
+        planeNP = self.render.attachNewNode(CollisionNode('planecnode'))
+        planeNP.node().addSolid(cp)
+        planeNP.show()
+        
+        # adding collision node to our crosshair. Based on camera.
+        self.pickerNode=CollisionNode('crosshairraycnode')
+        self.crosshairNP=base.camera.attachNewNode(self.pickerNode)
+        self.crosshairRay=CollisionRay(Point3(0,0,0),Vec3(0,1,0))
+        self.pickerNode.addSolid(self.crosshairRay)
+        base.cTrav.addCollider(self.crosshairNP, collisionHandler)
+
+        # loading student_chair model
+        # first off we gotta define the topmost node that should be a PandaNode wrapped into a nodepath - this is mandatory cos if we try to directly use the  Actornode defined below, we'll face inexpected behavior manipulating the object.
+        self.studentChairNP=NodePath(PandaNode("phisicschair"))
+        # we then need an ActorNode - this is required when playing with physics cos it got an interface suitable for this task while the usual nodepath ain't. Then we'll stick it to the main nodepath we'll put into the scene render node, wrapped into a nodepath of course.
+        self.studentChairAN=ActorNode("chairactnode")
+        self.studentChairANP=self.studentChairNP.attachNewNode(self.studentChairAN)
         self.studentChairModel = loader.loadModel("../../models/student_chair")
-        studentChairCollider = self.studentChairModel.attachNewNode(CollisionNode('student_chaircnode'))
-        studentChairCollider.node().addSolid(CollisionSphere(0, 0, 0, 1))
-        self.studentChairModel.reparentTo(render)
-        self.studentChairModel.setPos(-2, 25,0)
+        self.studentChairModel.reparentTo(self.studentChairANP)
+        self.studentChairModel.setPos(-2, 25,50)
+        self.studentChairModel.setHpr(10.0,13.0,5.0)
+        self.studentChairCollider = self.studentChairModel.attachNewNode(CollisionNode('student_chaircnode'))
+        self.studentChairCollider.node().addSolid(CollisionSphere(8, -5, 6, 10))
+        # now it's a good time to dip our object into the physics environment (the Actornode btw)
+        base.physicsMgr.attachPhysicalNode(self.studentChairAN)
+        # then tell to the PhysicsCollisionHandler what are its collider and main nodepath to handle - this means that the ballANP nodepath will be phisically moved to react to all the physics forces we applied in the environment (the gravity force in the specific). Note that due we are using a particular collison handler (PhysicsCollisionHandler) we cannot pass a common nodepath as we did in all the previous steps but a nodepath-wrapped Actornode.
+        physicsHandler.addCollider(self.studentChairCollider, self.studentChairANP)
+        # and inform the main traverser as well
+        base.cTrav.addCollider(self.studentChairCollider, physicsHandler)
+        # now the physic ball is ready to exit off the dispenser - Note we reparent it to render by default
+        self.studentChairNP.reparentTo(base.render)
+        # debug purposes - show collision solid
+#        self.studentChairCollider.show()
+        
+        # collision handler methods
+        def collideStudentChairIn(entry):
+            print "colisao de entrada"
+        def collideStudentChairOut(entry):
+            print "colisao de saida"
+        
+        # adding a pattern - eases readability
+        collisionHandler.addInPattern('%fn-into-%in')
+        collisionHandler.addOutPattern('%fn-out-%in')
+
+        # accepting student chair collision
+        self.accept('crosshairraycnode-into-student_chaircnode', collideStudentChairIn)
+        self.accept('crosshairraycnode-out-student_chaircnode', collideStudentChairOut)
         
         
 main = Main()
