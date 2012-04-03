f550645260ba4ac63b937a27fca32946dd136c43
guisenges@poli.ufrj.br <guisenges@poli.ufrj.br@53eebc65-bcf8-1c7c-f537-236fe9c61844>
2012-04-03 19:18:05 +0000

    Created HUD class.

---
+++ src/main/PlayerHUD.py
@@ -0,0 +1,7 @@
+from direct.showbase.ShowBase import ShowBase
+from main import *       
+
+class InputHandler(object):
+
+    def __init__(self, mainReference):
+        self.mainRef = mainReference
