769a74dd35591c4a1f58f849a941dbc5d4f71805
guisenges@poli.ufrj.br <guisenges@poli.ufrj.br@53eebc65-bcf8-1c7c-f537-236fe9c61844>
2012-04-03 20:06:29 +0000

    Began implementing crosshair HUD.

--- src/main/PlayerHUD.py
+++ src/main/PlayerHUD.py
@@ -1,7 +1,20 @@
-from direct.showbase.ShowBase import ShowBase
-from main import *       
+from pandac.PandaModules import NodePath
+from pandac.PandaModules import CardMaker       
 
-class InputHandler(object):
+class PlayerHUD(object):
 
     def __init__(self, mainReference):
         self.mainRef = mainReference
+        
+        cm = CardMaker('spritesMaker') 
+        cm.setFrame(-0.5, 0.5, -0.5, 0.5) 
+        sprite = cm.generate()    
+        
+        # crosshair init
+        tex = loader.loadTexture('crosshair.png') 
+        spriteNP = NodePath(sprite)    
+        spriteNP.setTexture(tex) 
+        spriteNP.setPos(0, 0, 0) 
+        spriteNP.setScale(1.0, 1.0, 1.0)
+        # enable transparency
+        spriteNP.setTransparency(1)
