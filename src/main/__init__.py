6b3fa8c2db49dc7c118aa219d26d240c1c914960
guisenges@poli.ufrj.br <guisenges@poli.ufrj.br@53eebc65-bcf8-1c7c-f537-236fe9c61844>
2012-04-11 04:36:07 +0000

    Adding a beta model of H-208 classroom.

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -74,10 +74,16 @@ class Main(ShowBase):
         myFog.setExpDensity(0.0007)
         render.setFog(myFog)
 
+        # loading h_208 room
+        self.h208Room = loader.loadModel("../../models/model_h208/h_208")
+        self.h208Room.reparentTo(self.render)
+        self.h208Room.setPos(0, 0, -27)
+        self.h208Room.setScale(34, 34, 34)
+
         # loading room
         self.testRoom = loader.loadModel("../../models/sala_teste")
         self.testRoom.reparentTo(self.render)
-        self.testRoom.setPos(0, 0, -27)
+        self.testRoom.setPos(500, 0, -27)
         self.testRoom.setScale(34, 34, 34)
         
         self.tex = loader.loadTexture("../../models/floorlm.png")
